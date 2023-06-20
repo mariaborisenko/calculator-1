08f48d1adf3f463157f427283ff8ec31f8ed673b
Ben Attanasio <benattanasio95@gmail.com>
2023-06-19 23:48:38 -0400

    First commit

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,12 +1,9 @@
 import React from "react";
-import { Textfit } from "react-textfit";
 import "./Screen.css";
 
 const Screen = ({ value }) => {
   return (
-    <Textfit className="screen" mode="single" max={70}>
       {value}
-    </Textfit>
   );
 };
 
